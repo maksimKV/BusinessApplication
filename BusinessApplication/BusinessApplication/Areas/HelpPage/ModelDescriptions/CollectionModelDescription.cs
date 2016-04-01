a1d674947df4952037a897538eac9eed2ca3a173
Maksim Kanev <maks_kv@yahoo.com>
2016-04-01 10:22:43 +0300

    Initial Commit

---
+++ BusinessApplication/BusinessApplication/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BusinessApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BusinessApplication/BusinessApplication/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BusinessApplication.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
