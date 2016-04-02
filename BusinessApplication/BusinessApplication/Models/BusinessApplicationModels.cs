2f0257b0430f38dedd0d1d522e4a06e043c5dc27
Maksim Kanev <maks_kv@yahoo.com>
2016-04-02 16:28:21 +0300

    Added the models for the application

---
+++ BusinessApplication/BusinessApplication/Models/BusinessApplicationModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessApplication.Models
+{
+    public class BusinessApplicationModels
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Employee : BusinessApplicationModels
+    {
+        public string Position { get; set; }
+        public Employee Supervisor { get; set; }
+        public List<Partner> Partners { get; set; }
+    }
+
+    public class Partner : BusinessApplicationModels
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public List<Employee> Partners { get; set; }
+    }
+}
