7c2eae68e6352dc5e424f9b29556c57982b28ddf
Maksim Kanev <maks_kv@yahoo.com>
2016-04-03 20:43:39 +0300

    Created the view all employees functionality

--- BusinessApplication/BusinessApplication/Controllers/EmployeeController.cs
+++ BusinessApplication/BusinessApplication/Controllers/EmployeeController.cs
@@ -4,10 +4,91 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using BusinessApplication;
 
 namespace BusinessApplication.Controllers
 {
     public class EmployeeController : ApiController
     {
+        [Route("employees")]
+        [HttpGet]
+        public IHttpActionResult AllEmployees()
+        {
+            List<Object> allEmployees = new List<Object>();
+
+            using (var context = new BusinessDBEntities())
+            {
+                List<Employee> values = context.Employees.ToList();
+
+                foreach (Employee value in values)
+                {
+
+                    List<Object> connections = new List<Object>();
+
+                    foreach (Connection connection in value.Connections)
+                    {
+                        Object partner = new
+                        {
+                            ID = connection.Partner.ID,
+                            Name = connection.Partner.Name,
+                            Phone = connection.Partner.Phone,
+                            Email = connection.Partner.Email
+                        };
+
+                        connections.Add(partner);
+                    }
+
+                    Object supervisor = null;
+                    List<Object> employees = new List<Object>();
+
+                    // Getting the supervisor
+                    if (value.Employee1 != null)
+                    {
+                        supervisor = new
+                        {
+                            ID = value.Employee1.ID,
+                            Name = value.Employee1.Name,
+                            Position = value.Employee1.Position
+                        };
+                    }
+
+                    // Getting the subordunates, if any
+                    if (value.Employees1 != null)
+                    {
+                        foreach (Employee subordinate in value.Employees1)
+                        {
+                            Object assistant = new
+                            {
+                                ID = subordinate.ID,
+                                Name = subordinate.Name,
+                                Position = subordinate.Position
+                            };
+
+                            employees.Add(assistant);
+                        }
+                    }
+
+                    Object employee = new
+                    {
+                        ID = value.ID,
+                        Name = value.Name,
+                        Position = value.Position,
+                        Supervisor = supervisor,
+                        Subordinates = employees,
+                        Partners = connections
+                    };
+
+                    allEmployees.Add(employee);
+                }
+            }
+            return Ok(allEmployees);
+        }
+
+        [Route("employees/remove/{id}")]
+        [HttpGet]
+        public void RemoveEmployee(int id)
+        {
+
+        }
     }
 }
