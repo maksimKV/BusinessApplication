57c45fd2013ec6ece8b9b03d68648615c4e9e4c8
Maksim Kanev <maks_kv@yahoo.com>
2016-04-14 22:34:49 +0300

    Added the routing for the pages

--- BusinessApplication/BusinessApplication/App/app.js
+++ BusinessApplication/BusinessApplication/App/app.js
@@ -1,5 +1,31 @@
 (function(){
 
-    var app = angular.module('BusinessApplication', []);
+    var app = angular.module('BusinessApplication', ['ngRoute']);
+
+    // Configuring application routes
+    app.config(function ($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/home', {
+                templateUrl: 'App/Pages/Home.html',
+                controller: 'AppController'
+            })
+
+            // route for the employees page
+            .when('/employees', {
+                templateUrl: 'App/Pages/Employees.html',
+                controller: 'AppController'
+            })
+
+            // route for the partners page
+            .when('/partners', {
+                templateUrl: 'App/Pages/Partners.html',
+                controller: 'AppController'
+            })
+
+            // default route
+            .otherwise({redirectTo: '/home'});
+    });
 	
 }());
