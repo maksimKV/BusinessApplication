98c6ba57efbf2c29c2621a6cd420f4ff8ed063f2
Maksim Kanev <maks_kv@yahoo.com>
2016-04-20 23:30:57 +0300

    Added some functions for the form checkboxes

--- BusinessApplication/BusinessApplication/App/Controllers/AppController.js
+++ BusinessApplication/BusinessApplication/App/Controllers/AppController.js
@@ -21,6 +21,10 @@ app.controller('AppController', ['$scope', '$log', '$routeParams', '$location', 
 	        GetPartners();
 	    }
 
+	    $scope.$watch('employee', function () {
+	        console.log($scope.employee);
+	    });
+
 	    $scope.$watch('routeParams', function () {
 	        if(typeof $routeParams.employeeID !== 'undefined')
 	        {
@@ -55,6 +59,75 @@ app.controller('AppController', ['$scope', '$log', '$routeParams', '$location', 
 	    $scope.RemovePartner = function (partnerID) {
 	        RequestService.RemovePartner(partnerID);
 	    };
+
+	    $scope.UpdateEmployee = function () {
+
+	    };
+
+	    $scope.Check = function (ID, Relationships) {
+	        var checked = false;
+
+	        for (var i = 0; i < Relationships.length; i++) {
+	            if (Relationships[i].ID == ID) {
+	                checked = true;
+	            }
+	        }
+
+	        return checked;
+	    };
+
+	    $scope.AddRemoveSubordinate = function (Object) {
+	        for (var i = 0; i < $scope.employee.Subordinates.length; i++) {
+	            if ($scope.employee.Subordinates[i].ID == Object.ID) {
+	                $scope.employee.Subordinates.splice(i, 1);
+
+	                return;
+	            }
+	        }
+
+	        $scope.employee.Subordinates.push(Object);
+	    };
+
+	    $scope.AddRemoveSupervisor = function (Object) {
+	        for (var i = 0; i < $scope.employee.Supervisor.length; i++) {
+	            if ($scope.employee.Supervisor[i].ID == Object.ID) {
+	                $scope.employee.Supervisor.splice(i, 1);
+
+	                return;
+	            }
+	        }
+
+	        $scope.employee.Supervisor.push(Object);
+	    };
+
+	    $scope.AddRemovePartnerFromEmployee = function (Object) {
+	        for (var i = 0; i < $scope.employee.Partners.length; i++) {
+	            if ($scope.employee.Partners[i].ID == Object.ID) {
+	                $scope.employee.Partners.splice(i, 1);
+
+	                return;
+	            }
+	        }
+
+	        $scope.employee.Partners.push(Object);
+	    }
+
+	    $scope.AddRemoveEmployeeFromPartner = function (Object) {
+	        for (var i = 0; i < $scope.partner.Partners.length; i++) {
+	            if ($scope.partner.Partners[i].ID == Object.ID) {
+	                $scope.partner.Partners.splice(i, 1);
+
+	                return;
+	            }
+	        }
+
+	        $scope.partner.Partners.push(Object);
+	    }
+
+	    $scope.UpdateEmployee = function () {
+	        console.log("Update Employee");
+	        console.log($scope.employee);
+	    }
 
 	    var GetEmployees = function () {
 	        RequestService.AllEmployees().then(function (data) {
