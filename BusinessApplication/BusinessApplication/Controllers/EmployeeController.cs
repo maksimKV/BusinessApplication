7ffe3f70a7972fd0b4068d9056c306aeab44bb2e
Maksim Kanev <maks_kv@yahoo.com>
2016-04-21 10:11:39 +0300

    Added the update employee method to the API

--- BusinessApplication/BusinessApplication/Controllers/EmployeeController.cs
+++ BusinessApplication/BusinessApplication/Controllers/EmployeeController.cs
@@ -5,6 +5,8 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using BusinessApplication;
+using Newtonsoft.Json.Linq;
+using System.Collections;
 
 namespace BusinessApplication.Controllers
 {
@@ -83,6 +85,66 @@ namespace BusinessApplication.Controllers
                 context.Employees.Remove(employee);
                 context.SaveChanges();
 
+                return Ok();
+            }
+        }
+
+        [Route("employees/update")]
+        [HttpPost]
+        public IHttpActionResult UpdateEmployee(JObject employee)
+        {
+            int employeeID = Convert.ToInt32(employee["ID"].ToString());
+            string employeeName = employee["Name"].ToString();
+            string employeePosition = employee["Position"].ToString();
+
+            JArray subordinates = employee["Subordinates"] as JArray;
+            JArray supervisors = employee["Supervisor"] as JArray;
+            JArray partners = employee["Partners"] as JArray;
+
+            using (var context = new BusinessDBEntities())
+            {
+                Employee employeeToUpdate = context.Employees.Find(employeeID);
+                employeeToUpdate.Name = employeeName;
+                employeeToUpdate.Position = employeePosition;
+
+                // Removing employee dependencies
+                context.Supervisors.RemoveRange(context.Supervisors.Where(x => x.SupervisorID == employeeID));
+                context.Supervisors.RemoveRange(context.Supervisors.Where(x => x.EmployeeID == employeeID));
+                context.Connections.RemoveRange(context.Connections.Where(x => x.EmployeeID == employeeID));
+
+                // Adding new employee dependencies
+                foreach (JObject supervisor in supervisors)
+                {
+                    context.Supervisors.Add(
+                        new Supervisor {
+                            SupervisorID = Convert.ToInt32(supervisor["ID"].ToString()),
+                            EmployeeID = employeeID
+                        });
+                }
+
+                // Adding new employee dependencies
+                foreach (JObject subordinate in subordinates)
+                {
+                    context.Supervisors.Add(
+                        new Supervisor
+                        {
+                            SupervisorID = employeeID,
+                            EmployeeID = Convert.ToInt32(subordinate["ID"].ToString())
+                        });
+                }
+
+                // Adding new employee dependencies
+                foreach (JObject partner in partners)
+                {
+                    context.Connections.Add(
+                        new Connection
+                        {
+                            EmployeeID = employeeID,
+                            PartnerID = Convert.ToInt32(partner["ID"].ToString())
+                        });
+                }
+
+                context.SaveChanges();
                 return Ok();
             }
         }
