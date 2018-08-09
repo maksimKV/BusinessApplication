99c914f6e749c2d9af438467e0ded9b10283e19d
Maksim Kanev <maks_kv@yahoo.com>
2018-08-09 18:14:54 +0300

    Updated the libraries

--- BusinessApplication/BusinessApplication/BusinessApplication.Designer.cs
+++ BusinessApplication/BusinessApplication/BusinessApplication.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\xampp\htdocs\BusinessApplication\BusinessApplication\BusinessApplication\BusinessApplication.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Projects\BusinessApplication\BusinessApplication\BusinessApplication\BusinessApplication.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
